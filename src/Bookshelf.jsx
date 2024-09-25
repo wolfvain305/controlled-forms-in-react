3c4d1327fc64fd42175b0817e95651ea67f154ba
DrueWolf <dwolf0305@gmail.com>
2024-09-24 20:56:47 -0400

    book handler

--- src/Bookshelf.jsx
+++ src/Bookshelf.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 
 const BookShelf =() => {
-const [books, setBook] = useState([])
+const [books, setBooks] = useState([])
 const [title, setTitle] = useState('')
 const [author, setAuthor] = useState('')
 
-    
+const handleAddBook = (e) => {
+    e.preventDefault()
+    if (title && author) {
+        const newBook = { title, author }
+        setBooks([...books, newBook])
+        setTitle('')
+        setAuthor('')
+    }
 }
-
+return (
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a Book</h3>
@@ -16,4 +23,7 @@ const [author, setAuthor] = useState('')
   <div className="bookCardsDiv">{/* Book cards will display here */}</div>
 </div>
 
+)
+
 export default BookshelfD;
+}
